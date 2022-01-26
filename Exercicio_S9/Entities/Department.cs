95ca973c96f9dd30517725e7e71e2baef1bb98ce
matheus.cruz <matheus.cruz@TPCP06INF020345>
2022-01-26 13:59:11 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_S9/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio_S9.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Exercicio_S9/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio_S9.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
